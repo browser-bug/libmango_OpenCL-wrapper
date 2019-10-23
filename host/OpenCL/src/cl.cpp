5d7d22cf4dc3fee91d7b434dfb4ec9f38fccc85f
Steven Salazar <stevensalazarmolina@gmail.com>
2019-10-23 20:37:42 +0000

    cl.cpp now contains definitions of two structs (just to see how it works)

--- host/OpenCL/src/cl.cpp
+++ host/OpenCL/src/cl.cpp
@@ -1,13 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "cl.h"
 
 #ifdef __cplusplus
-#include <stdio.h>
-#endif
+ extern "C" {
+#endif  
+	
+    // struct for platform_id, just used to verify that it will work
+	struct _cl_platform_id
+	{
+		//struct _cl_icd_dispatch const *const icd_dispatch;
+		//cl_bool initialized;
+		struct _cl_device_id *devices;
+	};
 
-extern "C"
-{
-    void displayString()
+
+    // same for device, also the device will contain an info that will be shown
+	struct _cl_device_id
+	{	
+	char  *ciao;
+	};
+
+    // so we have a device
+	struct _cl_device_id device0;
+
+
+    // init connects the devices with the user's variable
+	void init(cl_device_id * device){
+			device[0] = &device0;
+			device0.ciao = "AABB";
+            
+            // a print to show that users data can be shown
+			printf("%s\n", ((device[0])->ciao) );
+
+	}
+
+
+ 	void get_device_info(cl_device_id * device)
     {
-        printf("PROVA COMPILATA!\n");
+            // after init we call this function and we should be able to see the info about the device anyway since we already got a reference to it
+			printf("%s\n", ((device[0])->ciao) );
+
+
     }
-}
+#ifdef __cplusplus
+ }
+#endif     
+
+
+
+
+
+
