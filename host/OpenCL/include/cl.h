4b3acd9efd1dc6de4ffbd7a91f1c5345fd40e97e
Steven Salazar <stevensalazarmolina@gmail.com>
2019-10-23 20:42:34 +0000

    cl.h now contains two functions that will be called (using the struct pointers)

--- host/OpenCL/include/cl.h
+++ host/OpenCL/include/cl.h
@@ -1,26 +1,21 @@
 #ifndef __OPENCL_CL_H
 #define __OPENCL_CL_H
 
-#ifdef __APPLE__
-#include <OpenCL/cl_platform.h>
-#else
-#include <cl_platform.h>
-#endif	
 
 // Including libmango
-#include "mango.h"
+ 
+ #include "mango.h"
 
 #ifdef __cplusplus
 extern "C"{
 #endif
 
-// Compiling test
-void displayString();
+typedef signed   int        cl_int;
 
+    // Compiling test
+    void displayString();
 
-#ifdef __cplusplus
-}
-#endif
+	
 
 // #ifdef __cplusplus
 // extern "C" {
@@ -28,9 +23,12 @@ void displayString();
 
 // /******************************************************************************/
 
-// typedef struct _cl_platform_id *    cl_platform_id;
-// typedef struct _cl_device_id *      cl_device_id;
-// typedef struct _cl_context *        cl_context;
+ typedef struct _cl_platform_id *    cl_platform_id;
+ typedef struct _cl_device_id *      cl_device_id;
+ typedef struct _cl_context *        cl_context;
+
+
+	void init(cl_device_id * device );
 // typedef struct _cl_command_queue *  cl_command_queue;
 // typedef struct _cl_mem *            cl_mem;
 // typedef struct _cl_program *        cl_program;
@@ -108,77 +106,76 @@ void displayString();
 // /******************************************************************************/
 
 // /* Error Codes */
-// #define CL_SUCCESS                                  0
-// #define CL_DEVICE_NOT_FOUND                         -1
-// #define CL_DEVICE_NOT_AVAILABLE                     -2
-// #define CL_COMPILER_NOT_AVAILABLE                   -3
-// #define CL_MEM_OBJECT_ALLOCATION_FAILURE            -4
-// #define CL_OUT_OF_RESOURCES                         -5
-// #define CL_OUT_OF_HOST_MEMORY                       -6
-// #define CL_PROFILING_INFO_NOT_AVAILABLE             -7
-// #define CL_MEM_COPY_OVERLAP                         -8
-// #define CL_IMAGE_FORMAT_MISMATCH                    -9
-// #define CL_IMAGE_FORMAT_NOT_SUPPORTED               -10
-// #define CL_BUILD_PROGRAM_FAILURE                    -11
-// #define CL_MAP_FAILURE                              -12
-// #define CL_MISALIGNED_SUB_BUFFER_OFFSET             -13
-// #define CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST -14
-// #define CL_COMPILE_PROGRAM_FAILURE                  -15
-// #define CL_LINKER_NOT_AVAILABLE                     -16
-// #define CL_LINK_PROGRAM_FAILURE                     -17
-// #define CL_DEVICE_PARTITION_FAILED                  -18
-// #define CL_KERNEL_ARG_INFO_NOT_AVAILABLE            -19
+ #define CL_SUCCESS                                  0
+ #define CL_DEVICE_NOT_FOUND                         -1
+ #define CL_DEVICE_NOT_AVAILABLE                     -2
+ #define CL_COMPILER_NOT_AVAILABLE                   -3
+ #define CL_MEM_OBJECT_ALLOCATION_FAILURE            -4
+ #define CL_OUT_OF_RESOURCES                         -5
+ #define CL_OUT_OF_HOST_MEMORY                       -6
+ #define CL_PROFILING_INFO_NOT_AVAILABLE             -7
+ #define CL_MEM_COPY_OVERLAP                         -8
+ #define CL_IMAGE_FORMAT_MISMATCH                    -9
+ #define CL_IMAGE_FORMAT_NOT_SUPPORTED               -10
+ #define CL_BUILD_PROGRAM_FAILURE                    -11
+ #define CL_MAP_FAILURE                              -12
+ #define CL_MISALIGNED_SUB_BUFFER_OFFSET             -13
+ #define CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST -14
+ #define CL_COMPILE_PROGRAM_FAILURE                  -15
+ #define CL_LINKER_NOT_AVAILABLE                     -16
+ #define CL_LINK_PROGRAM_FAILURE                     -17
+ #define CL_DEVICE_PARTITION_FAILED                  -18
+ #define CL_KERNEL_ARG_INFO_NOT_AVAILABLE            -19
 
-// #define CL_INVALID_VALUE                            -30
-// #define CL_INVALID_DEVICE_TYPE                      -31
-// #define CL_INVALID_PLATFORM                         -32
-// #define CL_INVALID_DEVICE                           -33
-// #define CL_INVALID_CONTEXT                          -34
-// #define CL_INVALID_QUEUE_PROPERTIES                 -35
-// #define CL_INVALID_COMMAND_QUEUE                    -36
-// #define CL_INVALID_HOST_PTR                         -37
-// #define CL_INVALID_MEM_OBJECT                       -38
-// #define CL_INVALID_IMAGE_FORMAT_DESCRIPTOR          -39
-// #define CL_INVALID_IMAGE_SIZE                       -40
-// #define CL_INVALID_SAMPLER                          -41
-// #define CL_INVALID_BINARY                           -42
-// #define CL_INVALID_BUILD_OPTIONS                    -43
-// #define CL_INVALID_PROGRAM                          -44
-// #define CL_INVALID_PROGRAM_EXECUTABLE               -45
-// #define CL_INVALID_KERNEL_NAME                      -46
-// #define CL_INVALID_KERNEL_DEFINITION                -47
-// #define CL_INVALID_KERNEL                           -48
-// #define CL_INVALID_ARG_INDEX                        -49
-// #define CL_INVALID_ARG_VALUE                        -50
-// #define CL_INVALID_ARG_SIZE                         -51
-// #define CL_INVALID_KERNEL_ARGS                      -52
-// #define CL_INVALID_WORK_DIMENSION                   -53
-// #define CL_INVALID_WORK_GROUP_SIZE                  -54
-// #define CL_INVALID_WORK_ITEM_SIZE                   -55
-// #define CL_INVALID_GLOBAL_OFFSET                    -56
-// #define CL_INVALID_EVENT_WAIT_LIST                  -57
-// #define CL_INVALID_EVENT                            -58
-// #define CL_INVALID_OPERATION                        -59
-// #define CL_INVALID_GL_OBJECT                        -60
-// #define CL_INVALID_BUFFER_SIZE                      -61
-// #define CL_INVALID_MIP_LEVEL                        -62
-// #define CL_INVALID_GLOBAL_WORK_SIZE                 -63
-// #define CL_INVALID_PROPERTY                         -64
-// #define CL_INVALID_IMAGE_DESCRIPTOR                 -65
-// #define CL_INVALID_COMPILER_OPTIONS                 -66
-// #define CL_INVALID_LINKER_OPTIONS                   -67
-// #define CL_INVALID_DEVICE_PARTITION_COUNT           -68
+ #define CL_INVALID_VALUE                            -30
+ #define CL_INVALID_DEVICE_TYPE                      -31
+ #define CL_INVALID_PLATFORM                         -32
+ #define CL_INVALID_DEVICE                           -33
+ #define CL_INVALID_CONTEXT                          -34
+ #define CL_INVALID_QUEUE_PROPERTIES                 -35
+ #define CL_INVALID_COMMAND_QUEUE                    -36
+ #define CL_INVALID_HOST_PTR                         -37
+ #define CL_INVALID_MEM_OBJECT                       -38
+ #define CL_INVALID_IMAGE_FORMAT_DESCRIPTOR          -39
+ #define CL_INVALID_IMAGE_SIZE                       -40
+ #define CL_INVALID_SAMPLER                          -41
+ #define CL_INVALID_BINARY                           -42
+ #define CL_INVALID_BUILD_OPTIONS                    -43
+ #define CL_INVALID_PROGRAM                          -44
+ #define CL_INVALID_PROGRAM_EXECUTABLE               -45
+ #define CL_INVALID_KERNEL_NAME                      -46
+ #define CL_INVALID_KERNEL_DEFINITION                -47
+ #define CL_INVALID_KERNEL                           -48
+ #define CL_INVALID_ARG_INDEX                        -49
+ #define CL_INVALID_ARG_VALUE                        -50
+ #define CL_INVALID_ARG_SIZE                         -51
+ #define CL_INVALID_KERNEL_ARGS                      -52
+ #define CL_INVALID_WORK_DIMENSION                   -53
+ #define CL_INVALID_WORK_GROUP_SIZE                  -54
+ #define CL_INVALID_WORK_ITEM_SIZE                   -55
+ #define CL_INVALID_GLOBAL_OFFSET                    -56
+ #define CL_INVALID_EVENT_WAIT_LIST                  -57
+ #define CL_INVALID_EVENT                            -58
+ #define CL_INVALID_OPERATION                        -59
+ #define CL_INVALID_GL_OBJECT                        -60
+ #define CL_INVALID_BUFFER_SIZE                      -61
+ #define CL_INVALID_MIP_LEVEL                        -62
+ #define CL_INVALID_GLOBAL_WORK_SIZE                 -63
+ #define CL_INVALID_PROPERTY                         -64
+ #define CL_INVALID_IMAGE_DESCRIPTOR                 -65
+ #define CL_INVALID_COMPILER_OPTIONS                 -66
+ #define CL_INVALID_LINKER_OPTIONS                   -67
+ #define CL_INVALID_DEVICE_PARTITION_COUNT           -68
+/* OpenCL Version */
+ #define CL_VERSION_1_0                              1
+ #define CL_VERSION_1_1                              1
+ #define CL_VERSION_1_2                              1
 
-// /* OpenCL Version */
-// #define CL_VERSION_1_0                              1
-// #define CL_VERSION_1_1                              1
-// #define CL_VERSION_1_2                              1
-
-// /* cl_bool */
-// #define CL_FALSE                                    0
-// #define CL_TRUE                                     1
-// #define CL_BLOCKING                                 CL_TRUE
-// #define CL_NON_BLOCKING                             CL_FALSE
+/* cl_bool */
+ #define CL_FALSE                                    0
+ #define CL_TRUE                                     1
+ #define CL_BLOCKING                                 CL_TRUE
+ #define CL_NON_BLOCKING                             CL_FALSE
 
 // /* cl_platform_info */
 // #define CL_PLATFORM_PROFILE                         0x0900
@@ -186,8 +183,8 @@ void displayString();
 // #define CL_PLATFORM_NAME                            0x0902
 // #define CL_PLATFORM_VENDOR                          0x0903
 // #define CL_PLATFORM_EXTENSIONS                      0x0904
-
-// /* cl_device_type - bitfield */
+ 
+ /* cl_device_type - bitfield */
 // #define CL_DEVICE_TYPE_DEFAULT                      (1 << 0)
 // #define CL_DEVICE_TYPE_CPU                          (1 << 1)
 // #define CL_DEVICE_TYPE_GPU                          (1 << 2)
@@ -558,6 +555,9 @@ void displayString();
 
 // /********************************************************************************************************/
 
+
+
+ 	void get_device_info(cl_device_id * device );
 // /* Platform API */
 // extern CL_API_ENTRY cl_int CL_API_CALL
 // clGetPlatformIDs(cl_uint          /* num_entries */,
@@ -1219,9 +1219,9 @@ void displayString();
 // extern CL_API_ENTRY CL_EXT_PREFIX__VERSION_1_1_DEPRECATED void * CL_API_CALL
 // clGetExtensionFunctionAddress(const char * /* func_name */) CL_EXT_SUFFIX__VERSION_1_1_DEPRECATED;
 
-// #ifdef __cplusplus
-// }
-// #endif
+ #ifdef __cplusplus
+}
+ #endif
 
 #endif  /* __OPENCL_CL_H */
 
