00a277ed7ea7805d11c41f8e3e85d4a5921a5888
Steven Salazar <stevensalazarmolina@gmail.com>
2019-10-29 22:50:26 +0000

    cl.cpp removed mycontext, my program and mykernel as global variables

--- host/OpenCL/src/cl.cpp
+++ host/OpenCL/src/cl.cpp
@@ -10,8 +10,6 @@
 struct _cl_context
 {
     cl_program p;
-    // cl_kern k;
-    // cl_mem m;
     mango_task_graph_t *tg;
 };
 
@@ -19,16 +17,19 @@ struct _cl_program
 {
     kernelfunction *kernfunc;
     mango_task_graph_t *tg;
-    // FIX maybe it has to be moved
+    // TO BE FIXED: maybe it has to be moved
     mango_kernel_t kernel;
 };
 
+
+// data is used  for the User's input data, in the case of a matrix multiplication it should contain a pointer to the matrix.
 struct _cl_mem
 {
     uint32_t id;
     mango_buffer_t buffer;
     void * data;
 };
+
 
 struct _cl_kernel
 {
@@ -45,41 +46,43 @@ struct _cl_platform_id
 
 
 struct _cl_device_id
-{	
-
+{	 
+  // TO BE FIXED: maybe it should have at least an ID
 };
+
 struct _cl_command_queue
 {
-
+  // TO BE FIXED: maybe it should have at least an ID
 };
+
 struct _cl_event
 {
-
+  // TO BE FIXED: maybe it should have at least an ID
 };
 
 struct _cl_sampler
 {
-
+  // TO BE FIXED: maybe it should have at least an ID
 };
 
 
      
 cl_int clGetPlatformIDs(cl_uint num_entries, cl_platform_id *entries, cl_uint *num_platforms){
-	// to be implemented
-	return CL_SUCCESS;}
+	// TO BE IMPLEMENTED
+	return CL_SUCCESS;
+}
+
 
 cl_int clGetDeviceIDs(cl_platform_id  platform,
                 cl_device_type    device_type , 
                 cl_uint           num_entries , 
                 cl_device_id *    devices , 
                 cl_uint *         num_devices ){
-	// to be implemented
-	return CL_SUCCESS;}
+	// TO BE IMPLEMENTED
+	return CL_SUCCESS;
+}
 
 
-
-cl_context mycontext = NULL;
-// this function allows to create a context
 cl_context clCreateContext(const cl_context_properties *  properties ,
                  cl_uint                  num_devices ,
                  const cl_device_id *     devices ,
@@ -90,18 +93,20 @@ cl_context clCreateContext(const cl_context_properties *  properties ,
         if (mango_init("test", "test_manga") == SUCCESS)
         {
             
+            cl_context mycontext = NULL;
             mycontext = (cl_context)malloc(sizeof(struct _cl_context));
             return mycontext;
         }
  }
 
 
-cl_command_queue clCreateCommandQueue(cl_context /* context */,
-                                      cl_device_id /* device */,
-                                      cl_command_queue_properties /* properties */,
-                                      cl_int * /* errcode_ret */)
+cl_command_queue clCreateCommandQueue(cl_context  context ,
+                                      cl_device_id device ,
+                                      cl_command_queue_properties  properties ,
+                                      cl_int *  errcode_ret )
 {
-    printf("CreateCommandQueue is not implemented\n");
+
+    // TO BE IMPLEMENTED
     cl_command_queue commands;
     commands = (cl_command_queue)malloc(sizeof(_cl_command_queue));
     
@@ -112,7 +117,6 @@ cl_command_queue clCreateCommandQueue(cl_context /* context */,
 
 
 
-cl_program myprogram = NULL;
 
 cl_program clCreateProgramWithBinary(cl_context                      context ,
                            cl_uint                         num_devices ,
@@ -120,18 +124,18 @@ cl_program clCreateProgramWithBinary(cl_context                      context ,
                            const size_t *                  lengths ,
                            const unsigned char **          binaries ,
                            cl_int *                        binary_status ,
-                           cl_int *                       errcode_ret ){
+                           cl_int *                       errcode_ret )
+{
 
+    cl_program myprogram = NULL;
+    myprogram = (cl_program)malloc(sizeof(struct _cl_program));
+    myprogram->kernfunc = mango_kernelfunction_init();
+    char kernel_file[] = "/opt/mango/usr/local/share/matrix_multiplication/matrix_multiplication_dev";
 
-
-     myprogram = (cl_program)malloc(sizeof(struct _cl_program));
-     myprogram->kernfunc = mango_kernelfunction_init();
-  char kernel_file[] = "/opt/mango/usr/local/share/matrix_multiplication/matrix_multiplication_dev";
-
-     // Associate program with context
-     mycontext->p = myprogram;
+    // Associate program with context
+    context->p = myprogram;
+    // load the kernel binary 
 	  mango_load_kernel(kernel_file, myprogram->kernfunc, GN, BINARY);
-
 
      return myprogram;
 }
@@ -160,7 +164,6 @@ cl_int  clBuildProgram(cl_program           program,
 
 
 
-cl_kernel mykernel;
 cl_kernel clCreateKernel(cl_program       program ,
                 const char *     kernel_name ,
                 cl_int *         errcode_ret ) {
@@ -168,19 +171,23 @@ cl_kernel clCreateKernel(cl_program       program ,
 
 	*errcode_ret = CL_SUCCESS;
 	 
+  cl_kernel mykernel;
 	mykernel = (cl_kernel) malloc(sizeof(_cl_kernel));
 
 	mykernel->id = KID;
 
-  mykernel->kernel = mango_register_kernel(mykernel->id, myprogram->kernfunc, 2, 1, B1, B2, B3);  
+  mykernel->kernel = mango_register_kernel(mykernel->id, program->kernfunc, 2, 1, B1, B2, B3);  
 
-	myprogram->tg = NULL;
-  myprogram->kernel = mykernel->kernel;
+	program->tg = NULL;
+  program->kernel = mykernel->kernel;
 	 return mykernel;
  
 }
 
 
+
+  // to be fixed, we would like to have a dynamic number of buffers. I mean it should know when to call mango_resource_allocation
+  // mango_task_graph_create will be fixed with mango_task_add_buffer and mango_task_add kernel
   int i=0;
   cl_mem d_C;
   cl_mem d_A;
@@ -197,38 +204,33 @@ cl_kernel clCreateKernel(cl_program       program ,
      if(i==3){
        	d_C = (cl_mem)malloc(sizeof(struct _cl_mem));
         d_C->id = i;
-       	printf("chiamando 3\n");
+        // host_ptr contains a pointer to the matrix C
         d_C->data  = host_ptr;
-        d_C->buffer = mango_register_memory(d_C->id, size, BUFFER, 1, 0, mycontext->p->kernel);
-        printf("chiamando 4\n");
+        d_C->buffer = mango_register_memory(d_C->id, size, BUFFER, 1, 0, context->p->kernel);
+       
+      	context->p->tg = mango_task_graph_create(1, 3, 0, context->p->kernel, d_A->buffer, d_B->buffer, d_C->buffer);
+       
+        mango_resource_allocation(context->p->tg);
 
-      	mycontext->p->tg = mango_task_graph_create(1, 3, 0, mycontext->p->kernel, d_A->buffer, d_B->buffer, d_C->buffer);
-        printf("chiamando 5\n");
-
-        mango_resource_allocation(mycontext->p->tg);
-
-        printf("chiamando 6\n");
-      	return d_C;
+       	return d_C;
 
      }else if(i==2){
-          printf("chiamando 2\n");
-     	    d_B = (cl_mem)malloc(sizeof(struct _cl_mem));
+          d_B = (cl_mem)malloc(sizeof(struct _cl_mem));
           d_B->id= i;
           d_B->data = host_ptr;
-          d_B->buffer = mango_register_memory(d_B->id, size, BUFFER, 0, 1, mycontext->p->kernel);
+          d_B->buffer = mango_register_memory(d_B->id, size, BUFFER, 0, 1, context->p->kernel);
 
           return d_B;
 
      }else if(i==1){
-        printf("chiamando 1\n");
      		d_A = (cl_mem)malloc(sizeof(struct _cl_mem));
         d_A->id = i;
         d_A->data = host_ptr;
-        d_A->buffer = mango_register_memory(d_A->id, size, BUFFER, 0, 1, mycontext->p->kernel);
+        d_A->buffer = mango_register_memory(d_A->id, size, BUFFER, 0, 1, context->p->kernel);
         return d_A;
 
      }else{
-     	printf("ERROR IN CREATED BUFFER\n");
+     	printf("ERROR IN CREATE BUFFER\n");
      }
 
 /*
@@ -244,18 +246,7 @@ cl_kernel clCreateKernel(cl_program       program ,
       }
 
 
-      /*
-            i++;
-            switch(i){
-            	case 1: b1 = memory;break;
-            	case 2: b2 = memory;break;
-            	case 3:
-      	        	//
-      	        	//i=0;
-      	        	break;
-            }
-            
-      /*        
+      
       	context->p->tg = mango_task_graph_add_kernel(NULL, &(kern->kernel));
       	context->p->tg = mango_task_graph_add_buffer(context->p->tg, &(memory->buffer));
 
@@ -267,6 +258,8 @@ cl_kernel clCreateKernel(cl_program       program ,
 
 
 
+
+  // TO BE FIXED: we should have a dynamic container that allows to have multiple args 
   /* Execution preparation */
   mango_arg_t *arg1 ;
   mango_arg_t *arg2 ;
@@ -306,18 +299,18 @@ cl_int clSetKernelArg(cl_kernel kernel,
        
     }
     if(arg_index == 0){
-      arg1 = mango_arg( mykernel->kernel, &(d_A->buffer), arg_size, arg_type);
+      arg1 = mango_arg( kernel->kernel, &(d_A->buffer), arg_size, arg_type);
     }else if(arg_index == 1){
-      arg2 = mango_arg( mykernel->kernel, &(d_B->buffer), arg_size, arg_type);
+      arg2 = mango_arg( kernel->kernel, &(d_B->buffer), arg_size, arg_type);
     }else if (arg_index == 2){
-      arg3 = mango_arg( mykernel->kernel, &(d_C->buffer), arg_size, arg_type);
+      arg3 = mango_arg( kernel->kernel, &(d_C->buffer), arg_size, arg_type);
     }else if (arg_index == 3 ){
-      arg4 = mango_arg( mykernel->kernel, value, arg_size, arg_type);
+      arg4 = mango_arg( kernel->kernel, value, arg_size, arg_type);
     }else if (arg_index == 4){
-      arg5 = mango_arg( mykernel->kernel, value, arg_size, arg_type);
+      arg5 = mango_arg( kernel->kernel, value, arg_size, arg_type);
       e= mango_get_buffer_event(d_C->buffer);
-      arg6 = mango_arg( mykernel->kernel, &e, sizeof(uint64_t), EVENT );
-      args =mango_set_args(mykernel->kernel, 6, arg1, arg2, arg3, arg4, arg5, arg6);
+      arg6 = mango_arg( kernel->kernel, &e, sizeof(uint64_t), EVENT );
+      args =mango_set_args(kernel->kernel, 6, arg1, arg2, arg3, arg4, arg5, arg6);
     }else{
       printf("ERROR in clSEtKErnelARG!!!\n");
     }
@@ -332,6 +325,8 @@ cl_int clSetKernelArg(cl_kernel kernel,
 
     return CL_SUCCESS;
 }
+
+
 mango_event_t ev;
  cl_int clEnqueueNDRangeKernel(cl_command_queue  command_queue ,
                         cl_kernel         kernel ,
@@ -341,52 +336,92 @@ mango_event_t ev;
                         const size_t *    local_work_size ,
                         cl_uint           num_events_in_wait_list ,
                         const cl_event *  event_wait_list ,
-                        cl_event *        event) {
-
-
+                        cl_event *        event) 
+ {
+  
   /* Data transfer host->device */
   mango_write(d_A->data, d_A->buffer, DIRECT, 0);
   mango_write(d_B->data, d_B->buffer, DIRECT, 0);
 
   /* spawn kernel */
-  ev = mango_start_kernel(mykernel->kernel, args, NULL);
-
-  /* reading results */
-  mango_wait(e);
-  mango_read(d_C->data, d_C->buffer, DIRECT, 0);
-
-  /* wait for kernel completion */
-  mango_wait(ev);
+  ev = mango_start_kernel(kernel->kernel, args, NULL);
 
 
-  /* shut down the mango infrastructure */
-  mango_resource_deallocation(mycontext->p->tg);
-  mango_task_graph_destroy_all(mycontext->p->tg);
-  mango_release();
+
+
+
   
  }
 
-/* 
-	struct _cl_device_id device0;
+
+ cl_int  clEnqueueReadBuffer(cl_command_queue     command_queue ,
+                     cl_mem               buffer ,
+                     cl_bool              blocking_read ,
+                     size_t               offset ,
+                     size_t               size , 
+                     void *               ptr ,
+                     cl_uint              num_events_in_wait_list ,
+                     const cl_event *     event_wait_list ,
+                     cl_event *           event )
+ {
+
+    /* reading results */
+    mango_wait(e);
+    mango_read(d_C->data, d_C->buffer, DIRECT, 0);
+
+    /* wait for kernel completion */
+    mango_wait(ev);
+
+    return CL_SUCCESS;
+
+ }
+
+                       
+ cl_int  clReleaseMemObject(cl_mem  memobj ) 
+ {
+  if(memobj != NULL)
+    free(memobj);
+  return CL_SUCCESS;
+ }
 
 
-	void init(cl_device_id * device){
-			device[0] = &device0;
-			device0.ciao = "AABB";
-			printf("%s\n", ((device[0])->ciao) );
+ cl_int  clReleaseProgram(cl_program  program )
+ {
 
-	}
+  /* shut down the mango infrastructure */
+  mango_resource_deallocation(program->tg);
+  mango_task_graph_destroy_all(program->tg);
+  free(program);
+ }
+
+ cl_int  clReleaseKernel(cl_kernel    kernel )
+ {
+  if(kernel != NULL)
+    free(kernel);
+  return CL_SUCCESS;
+ }
 
 
- 	void get_device_info(cl_device_id * device)
-    {
-			printf("%s\n", ((device[0])->ciao) );
+ cl_int  clReleaseCommandQueue(cl_command_queue  command_queue )
+ {
+  if(command_queue != NULL)
+    free(command_queue);
+  return CL_SUCCESS;
+ }
 
 
-    }
+ cl_int  clReleaseContext(cl_context context ) 
+ {
+
+  if(mango_release() != SUCCESS){
+    printf("PROBLEM WHILE RELEASING MANGO!\n");
+  }
+  if(context != NULL)
+    free(context);
+  return CL_SUCCESS;
+  }
 
 
-    */
 #ifdef __cplusplus
  }
 #endif     
