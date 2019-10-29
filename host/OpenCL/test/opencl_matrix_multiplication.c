d93c51ba589d764eb9b27f6b815ef66e35d07364
Steven Salazar <stevensalazarmolina@gmail.com>
2019-10-29 16:34:20 +0000

    example opencl, added more function calls to 'opencl' functions

--- host/OpenCL/test/opencl_matrix_multiplication.c
+++ host/OpenCL/test/opencl_matrix_multiplication.c
@@ -1,12 +1,321 @@
+////////////////////////////////////////////////////////////////////////////////
+
+#include <fcntl.h>
 #include <stdio.h>
-#include <cl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#include "cl.h"
+#include <stdbool.h>
 
-int main(){
+////////////////////////////////////////////////////////////////////////////////
+#define WA 4
+#define HA 4
+#define WB 4
 
-    size_t dataBytes;
-    cl_device_id *device = (cl_device_id *)malloc(dataBytes);
+#define HB WA
+#define WC WB
+#define HC HA
+////////////////////////////////////////////////////////////////////////////////
 
-    init(device);
-    get_device_info(device);
-    return 0;
+
+// Allocates a matrix with random float entries.
+void randomMemInit(float* data, int size)
+{
+   int i;
+
+   for (i = 0; i < size; ++i)
+    data[i] = rand() / (float)RAND_MAX;
 }
+
+long LoadOpenCLKernel(char const* path, char **buf)
+{
+    FILE  *fp;
+    size_t fsz;
+    long   off_end;
+    int    rc;
+
+    printf("1st if\n");
+    /* Open the file */
+    fp = fopen(path, "r");
+    if( NULL == fp ) {
+        return -1L;
+    }
+
+    printf("2nd if\n");
+    /* Seek to the end of the file */
+    rc = fseek(fp, 0L, SEEK_END);
+    if( 0 != rc ) {
+        return -1L;
+    }
+
+    printf("3rd if\n");
+    /* Byte offset to the end of the file (size) */
+    if( 0 > (off_end = ftell(fp)) ) {
+        return -1L;
+    }
+        printf("4rd if\n");
+
+    fsz = (size_t)off_end;
+
+    /* Allocate a buffer to hold the whole file */
+    *buf = (char *) malloc( fsz+1);
+    if( NULL == *buf ) {
+        return -1L;
+    }
+
+    /* Rewind file pointer to start of file */
+    rewind(fp);
+
+    /* Slurp file into buffer */
+    if( fsz != fread(*buf, 1, fsz, fp) ) {
+        free(*buf);
+        return -1L;
+    }
+
+    /* Close the file */
+    if( EOF == fclose(fp) ) {
+        free(*buf);
+        return -1L;
+    }
+
+
+    /* Make sure the buffer is NUL-terminated, just in case */
+    (*buf)[fsz] = '\0';
+
+    /* Return the file size */
+    return (long)fsz;
+}
+
+int main(int argc, char** argv)
+{
+   int err;                            // error code returned from api calls
+
+   cl_device_id device_id;             // compute device id
+   cl_context context;                 // compute context
+   cl_command_queue commands;          // compute command queue
+   cl_program program;                 // compute program
+   cl_kernel kernel;                   // compute kernel
+
+    // OpenCL device memory for matrices
+   cl_mem d_A;
+   cl_mem d_B;
+   cl_mem d_C;
+
+   // set seed for rand()
+   srand(2014);
+
+   //Allocate host memory for matrices A and B
+   unsigned int size_A = WA * HA;
+   unsigned int mem_size_A = sizeof(float) * size_A;
+   float* h_A = (float*) malloc(mem_size_A);
+
+   unsigned int size_B = WB * HB;
+   unsigned int mem_size_B = sizeof(float) * size_B;
+   float* h_B = (float*) malloc(mem_size_B);
+
+   //Initialize host memory
+   randomMemInit(h_A, size_A);
+   randomMemInit(h_B, size_B);
+
+
+
+   //Allocate host memory for the result C
+   unsigned int size_C = WC * HC;
+   unsigned int mem_size_C = sizeof(float) * size_C;
+   float* h_C = (float*) malloc(mem_size_C);
+
+   printf("Initializing OpenCL device...\n");
+
+   cl_uint dev_cnt = 0;
+   clGetPlatformIDs(0, 0, &dev_cnt);
+
+   cl_platform_id platform_ids[dev_cnt];
+   clGetPlatformIDs(dev_cnt, platform_ids, NULL);
+
+   // Connect to a compute device
+   int gpu = 1;
+   err = clGetDeviceIDs(platform_ids[0], gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
+   if (err != CL_SUCCESS)
+   {
+       printf("Error: Failed to create a device group!\n");
+       return EXIT_FAILURE;
+   }
+
+   // Create a compute context
+   context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+   if (!context)
+   {
+       printf("Error: Failed to create a compute context!\n");
+       return EXIT_FAILURE;
+   }else {
+      printf("Context has been created correctly\n");
+   }
+
+   
+   // Create a command commands
+   commands = clCreateCommandQueue(context, device_id, 0, &err);
+   if (!commands)
+   {
+       printf("Error: Failed to create a command commands!\n");
+       return EXIT_FAILURE;
+   }else {
+      printf("Commands has been created correctly\n");
+   }
+
+   /*
+     // Create the compute program from the source file
+     char *KernelSource;
+     long lFileSize;
+
+     // For simplicity change it to your absolute path.
+     lFileSize = LoadOpenCLKernel("/home/steven/Desktop/bernardo/libmango_test/host/OpenCL/test/kernel/kernel.cl", &KernelSource);
+     if( lFileSize < 0L ) {
+         perror("File2 read failed");
+         return 1;
+     }else{
+        printf("File read correctly\n");
+     }
+   */
+     
+
+     // it does nothing? i guess it should just load the kernel
+     program = clCreateProgramWithBinary(context, 1 , NULL, NULL, NULL, NULL, &err);
+     if (!program)
+     {
+       printf("Error: Failed to create compute program!\n");
+       return EXIT_FAILURE;
+    }else {
+      printf("Program has been created correctly\n");
+   }
+
+   
+
+   // Build the program executable
+   err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+   if (err != CL_SUCCESS)
+   {
+       size_t len;
+       char buffer[2048];
+       printf("Error: Failed to build program executable!\n");
+       //clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
+       printf("%s\n", buffer);
+       exit(1);
+   }else {
+      printf("Program has been built correctly\n");
+   }
+
+
+   // Create the compute kernel in the program we wish to run
+   //
+   kernel = clCreateKernel(program, "kernel_function", &err);
+   if (!kernel || err != CL_SUCCESS)
+   {
+       printf("Error: Failed to create compute kernel!\n");
+       exit(1);
+   }else {
+      printf("Kernel has been created correctly\n");
+   }
+
+
+   
+
+    // Create the input and output arrays in device memory for our calculation
+    d_A = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, mem_size_A, h_A, &err);
+    d_B = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, mem_size_B, h_B, &err);
+    
+    d_C = clCreateBuffer(context, CL_MEM_READ_WRITE, mem_size_A, h_C, &err);
+
+   if (!d_A || !d_B || !d_C)
+   {
+       printf("Error: Failed to allocate device memory!\n");
+       exit(1);
+   }else{
+    printf("OK WE DID ALLOCATE MEMORY\n");
+   }
+
+   printf("Running matrix multiplication for matrices A (%dx%d) and B (%dx%d) ...\n", WA,HA,WB,HB);
+
+
+
+   //Launch OpenCL kernel
+   size_t localWorkSize[2], globalWorkSize[2];
+
+   int wA = WA;
+   int wC = WC;
+   err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&d_C);
+   err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&d_A);
+   err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&d_B);
+   err |= clSetKernelArg(kernel, 3, sizeof(int), (void *)&wA);
+   err |= clSetKernelArg(kernel, 4, sizeof(int), (void *)&wC);
+
+   if (err != CL_SUCCESS)
+   {
+       printf("Error: Failed to set kernel arguments! %d\n", err);
+       exit(1);
+   }else{
+      printf("clSetKernelArg worked correctly\n");
+   }
+
+   localWorkSize[0] = 16;
+   localWorkSize[1] = 16;
+   globalWorkSize[0] = 1024;
+   globalWorkSize[1] = 1024;
+      printf("WRITE WORKED3\n");
+
+   err = clEnqueueNDRangeKernel(commands, kernel, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
+      printf("WRITE WORKED4\n");
+
+   if (err != CL_SUCCESS)
+   {
+       printf("Error: Failed to execute kernel! %d\n", err);
+       exit(1);
+   }else{
+      printf("WRITE WORKED\n");
+   }
+/*
+   //Retrieve result from device
+   err = clEnqueueReadBuffer(commands, d_C, CL_TRUE, 0, mem_size_C, h_C, 0, NULL, NULL);
+
+   if (err != CL_SUCCESS)
+   {
+       printf("Error: Failed to read output array! %d\n", err);
+       exit(1);
+   }
+
+   //print out the results
+   
+   /* printf("\n\nMatrix C (Results)\n");
+   int i;
+   for(i = 0; i < size_C; i++)
+   {
+      printf("%f ", h_C[i]);
+      if(((i + 1) % WC) == 0)
+      printf("\n");
+   }
+   printf("\n"); 
+  
+   printf("Matrix multiplication completed...\n");
+
+   //Shutdown and cleanup
+   free(h_A);
+   free(h_B);
+   free(h_C);
+
+   clReleaseMemObject(d_A);
+   clReleaseMemObject(d_C);
+   clReleaseMemObject(d_B);
+
+   clReleaseProgram(program);
+   clReleaseKernel(kernel);
+   clReleaseCommandQueue(commands);
+   clReleaseContext(context);
+
+*/
+   return 0;
+}
+
